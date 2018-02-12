419c2cae5e5da951883aeb2baa590112439ef094
Javier Hernán Caballero García <caballerojavier13@gmail.com>
2018-02-12 16:46:58 -0300

    Corrección url para ejecución en producción

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,6 @@
 module.exports = {
-    //url: 'http://bonita:8080/bonita/login.jsp',
-    url: 'http://localhost:35407/bonita/login.jsp',
+    url: 'http://bonita:8080/bonita/login.jsp',
+    //url: 'http://localhost:35407/bonita/login.jsp',
     elements: {
         username: {
             selector: '#username'
